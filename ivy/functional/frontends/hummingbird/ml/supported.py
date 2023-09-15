171afd2a6bc04685c79526852c3d02933eebd821
umairjavaid <bscs13024@itu.edu.pk>
2023-09-15 15:34:28 +0000

    original hb

--- ivy/functional/frontends/hummingbird/ml/supported.py
+++ ivy/functional/frontends/hummingbird/ml/supported.py
@@ -10,15 +10,15 @@ def _build_backend_map():
     backends = defaultdict(lambda: None)
 
     if torch_installed():
-        #import torch
-        import ivy.functional.frontends.torch as torch
+        import torch
+        #import ivy.functional.frontends.torch as torch
         backends[torch.__name__] = torch.__name__
         backends["py" + torch.__name__] = torch.__name__  # For compatibility with earlier versions.
 
         #backends[torch.jit.__name__] = torch.jit.__name__
         #backends["torchscript"] = torch.jit.__name__  # For reference outside Hummingbird.
 
-    return backends
+        return backends
 
 backends = _build_backend_map()
 
